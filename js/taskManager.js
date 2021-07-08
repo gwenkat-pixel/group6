532eea989ffe1e5db2fdd05901aadb792024c89b
Brent Bartlett <brent.bartlett1@gmail.com>
2021-07-08 14:59:07 -0700

    add addTask function

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,5 +1,19 @@
 class TaskManager {
-  constructor(tasks) {
+  constructor(tasks, currentId=0) {
     this.tasks = [];
+    this.currentId = currentId;
+  }
+  addTask(name, description, assignedTo, 
+    dueDate, status="TODO") {
+    this.currentId += 1;
+    const newTask = {
+      id: this.currentId,
+      name,
+      description,
+      assignedTo,
+      dueDate,
+      status
+    }
+    this.tasks.push(newTask);
   }
 }
