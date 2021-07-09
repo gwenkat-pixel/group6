27077b9d92d6f7d63fe6984d91f0f33d7ebafa3d
Brent Bartlett <brent.bartlett1@gmail.com>
2021-07-09 12:02:29 -0700

    get task form and add an event listener on submit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 const taskManager = new TaskManager();
-taskManager.addTask("Test addTask method",
+const taskForm = document.getElementById("task-form");
+taskForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
+/* taskManager.addTask("Test addTask method",
   "Write and test the addTask method on TaskManager",
   "Brent", "2021-07-08");
-console.log(taskManager.tasks);
+console.log(taskManager.tasks); */
